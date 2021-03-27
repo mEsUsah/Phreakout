6de4b53f023a07ee404245db426e6a6aa4bb389c
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2021-03-27 22:43:17 +0100

    Add ball

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,6 @@
 import Paddle from './components/paddle';
 import InputHandler from './components/input';
+import Ball from './components/ball';
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -10,12 +11,17 @@ const GAME_HEIGHT = 600;
 
 
 let paddle = new Paddle(GAME_WIDTH,GAME_HEIGHT);
+let ball = new Ball();
+
 new InputHandler(paddle);
 
 paddle.draw(ctx);
 
 let lastTime = 0;
 
+
+
+//Game - Main loop
 function gameLoop(timestamp){
     let deltatime = timestamp - lastTime;
     lastTime = timestamp
@@ -23,6 +29,12 @@ function gameLoop(timestamp){
     ctx.clearRect(0,0,800,600);
     paddle.update(deltatime);
     paddle.draw(ctx);
+
+    ball.draw(ctx);
+
+    
+
+
 
     requestAnimationFrame(gameLoop);
 }
