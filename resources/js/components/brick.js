415af119fd568f372d27b245a2c0da1eb4734c05
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2021-03-28 22:16:18 +0200

    Collisiondetection: added side hit detection, add this to brick

--- resources/js/components/brick.js
+++ resources/js/components/brick.js
@@ -12,15 +12,30 @@ export default class Brick {
         };
         this.position = position;
         this.width = 80;
-        this.height = 24;
+        this.height = 60;
 
         this.markedForDeletion = false;
     }
 
     update(){
-        if(detectCollision(this.game.ball, this)){
-            this.game.ball.speed.y = -this.game.ball.speed.y;
-            this.markedForDeletion = true;
+        let collision = detectCollision(this.game.ball, this);
+        switch(collision){
+            case "top":
+                this.game.ball.speed.y = -this.game.ball.speed.y;
+                this.markedForDeletion = true;
+                break;
+            case "bottom":
+                this.game.ball.speed.y = -this.game.ball.speed.y;
+                this.markedForDeletion = true;
+                break;
+            case "left":
+                this.game.ball.speed.x = -this.game.ball.speed.x;
+                this.markedForDeletion = true;
+                break;
+            case "right":
+                this.game.ball.speed.x = -this.game.ball.speed.x;
+                this.markedForDeletion = true;
+                break;
         }
     }
 
--- resources/js/components/collisionDetection.js
+++ resources/js/components/collisionDetection.js
@@ -1,19 +1,60 @@
-export function detectCollision(ball, gameObject){
+export function detectCollision(ballObject, gameObject){
     // Check collistion with the object
-    let bottomOfBall = ball.position.y + ball.size;
-    let topOfBall = ball.position.y;
+    let ball = {
+        top: ballObject.position.y,
+        bottom: ballObject.position.y + ballObject.size,
+        left: ballObject.position.x,
+        right: ballObject.position.x + ballObject.size,
+    }
+    let object = {
+        top: gameObject.position.y,
+        bottom: gameObject.position.y + gameObject.height,
+        left: gameObject.position.x,
+        right: gameObject.position.x + gameObject.width,
+    }
 
-    let topOfObject = gameObject.position.y;
-    let bottomOfObject = gameObject.position.y + gameObject.height;
-    let leftSideOfObject = gameObject.position.x;
-    let rightSideOfObject = gameObject.position.x + gameObject.width;
     
-    if(bottomOfBall >= topOfObject
-        && topOfBall <= bottomOfObject 
-        && ball.position.x >= leftSideOfObject
-        && ball.position.x + ball.size <= rightSideOfObject
+    
+    if(    ball.top <= object.bottom 
+        && ball.bottom >= object.top 
+        && ball.right >= object.left
+        && ball.left <= object.right
     ){
-        return true;
+        if(    ball.right   >=  object.left 
+            && ball.left    <   object.left 
+            && ball.bottom  >=  object.top 
+            && ball.top     <=  object.bottom 
+        ){
+            console.log("left");
+            return("left");
+        }
+        
+        else if(     ball.left   <=  object.right
+            &&  ball.right  >   object.right
+            &&  ball.bottom >=  object.top
+            &&  ball.top    <=  object.bottom
+        ){
+            console.log("right");
+            return("right");
+        }
+
+        else if(     ball.bottom >=  object.top 
+            &&  ball.top    <   object.top
+            &&  ball.right  >=  object.left
+            &&  ball.left   <=  object.right
+        ){
+            console.log("top");
+            return("top");
+        }
+
+        else if(     ball.top    <=  object.bottom 
+            &&  ball.bottom >   object.bottom
+            &&  ball.right  >=  object.left
+            &&  ball.left   <=  object.right
+        ){
+            console.log("bottom");
+            return("bottom");
+        } 
     } else {
         return false;
     }
